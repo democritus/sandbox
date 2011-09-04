fc1a99c34c6d0880ab847531aadbde18ffc8883f
Brian Warren <brian.scott.warren@gmail.com>
2011-09-04 01:28:09 -0500

    smacking the perl around like a bitch

--- multi_host_sync.pl
+++ multi_host_sync.pl
@@ -6,63 +6,94 @@ use warnings;
 use File::Basename;
 use YAML::XS;
 
+my $configuration = configuration( 'daily_sync.yaml' );
+sync( $configuration );
+
 sub sync {
-  say &sync_command &configuration{'targets'};
+  #my %configuration = shift @_;
+
+  print sync_command( %configuration );
 }
 
 sub sync_command {
+  #my %configuration = shift @_;
+
   my @commands = [];
-  my @actions = [ 'put' ];
-  foreach ( @actions ) {
+  my @targets = @configuration{'targets'};
+  foreach ( [ 'put' ] ) {
     my $action = $_;
-    foreach ( &targets ) {
-      @commands.push( eval("&$action_command", $_) );
+    foreach ( @targets ) {
+      @commands.push( eval($action_command . '($configuration{"filename"})') );
     }
   }
   @commands.join("; \\\n");
 }
 
 sub configuration {
-  $filename = shift @_;
-  %configuration_from_file = YAML::LoadFile( $filename );
-  return ( %default_options, %configuration_from_file );
-}
+  #my %configuration = shift @_;
 
-sub configuration_from_file {
-  return YAML::XS::LoadFile( @_[0] );
+  #%default_options = default_options();
+  #%options_from_file = YAML::XS::LoadFile( $configuration{'filename'} );
+  #return (
+  #  %default_options,
+  #  %options_from_file,
+  #  'filename' => $configuration{'filename'}
+  #);
+  return (
+    default_options(),
+    YAML::XS::LoadFile( $configuration{'filename'} ),
+    'filename' => $configuration{'filename'}
+  );
 }
 
 sub source_path {
-  return &configuration{ 'source_path' }
+  my %configuration = shift @_;
+
+  return $configuration{ 'source_path' }
 }
 
 sub base_name {
-  unless ( open SOURCE_FILE, '<', &source_path ) {
+  my %configuration = shift @_;
+
+  $source_path = source_path( %configuration );
+  unless ( open SOURCE_FILE, '<', $source_path ) {
     die "Cannot open source directory: $!";
   }
-  return File::Basename::basename( &source_path );
+  return File::Basename::basename( $source_path );
 }
 
 sub source_directory {
-  return File::Basename::dirname( &source_path );
+  my %configuration = shift @_;
+
+  return File::Basename::dirname( source_path( %configuration ) );
 }
 
 sub target_path {
-  my %target = shift @_;
-  &target_directory( %target ) + '/' + &base_name;
+  target_directory( shift @_ ) + '/' + base_name();
 }
 
 sub target_directory {
   my %target = shift @_;
-  %target{'user'} . '@' . %target{'host'} . ':' . %target{'directory'};
+  $target{'user'} . '@' . $target{'host'} . ':' . $target{'directory'};
 }
 
 sub get_command {
-  "rsync &options_list &source_path &target_directory(target)";
+  my $target = shift @_;
+  #my $options_list = options_list();
+  #my $source_path = target_path( $target );
+  #my $target_directory = source_directory( $target );
+  'rsync ' . options_list() . ' ' . source_path( $target ) . ' ' .
+    target_directory( $target );
 }
 
 sub put_command {
-  "rsync &options_list &target_path &source_directory(target)";
+  my $target = shift @_;
+  #my $options_list = options_list();
+  #my $target_path = target_path( $target );
+  #my $source_directory = source_directory( $target );
+  #"rsync $options_list $target_path $source_directory";
+  'rsync' . options_list() . ' ' . target_path( $target ) . ' ' .
+    source_directory( $target );
 }
 
 sub default_options {
@@ -79,6 +110,8 @@ sub default_options {
 
 sub options_list {
   my @list;
+  # TODO: merge options from file an default options
+  my %options = default_options();
   while ( ($key, $value) = each %options ) {
     if ( $value ) {
       @list.push( '--' . $key . '=' . $value );
@@ -86,5 +119,5 @@ sub options_list {
       @list.push( '--' . $key);
     }
   }
-  return list.join(' ');
+  return @list.join(' ');
 }

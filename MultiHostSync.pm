355636fd3945c6351e19a5d84e55592845a710fa
Brian Warren <brian.scott.warren@gmail.com>
2011-10-17 00:51:39 -0500

    enabled list of source files

--- MultiHostSync.pm
+++ MultiHostSync.pm
@@ -5,10 +5,10 @@ package MultiHostSync;
 use 5.010;
 use strict;
 use warnings;
-#use Cwd;
 use File::Basename;
 use YAML::XS;
 use Data::Dumper;
+use Sys::Hostname;
 
 # EXAMPLE
 # my $DailySync = MultiHostSync->new( 'daily_sync.yaml' );
@@ -16,7 +16,7 @@ use Data::Dumper;
 
 # Compile-time verified class fields
 # http://perldoc.perl.org/fields.html
-use fields qw( configuration exclude_patterns filename local_directory options targets );
+use fields qw( configuration exclude_patterns files hosts local_directory options );
 
 my %default_options = (
   'compress' => 1,
@@ -30,33 +30,44 @@ my %default_options = (
 
 sub new {
   my $self = shift;
-  my $filename = shift;
+  my $configuration_file = shift;
+  # TODO: get options array to permit setting any options from command line
   $self = fields::new($self) unless ref $self;
   # Load YAML into configuration hash
-  open my $fh, '<', $filename
+  open my $fh, '<', $configuration_file
     or die "can't open config file: $!";
   my $configuration = YAML::XS::LoadFile( $fh );
   $self->{configuration} = $configuration;
-  $self->{exclude_patterns} = @$configuration{'exclude_patterns'};
-  $self->{filename} = $filename;
-  $self->{local_directory} = @$configuration{'local_directory'};
-  $self->{options} = @$configuration{'options'};
-  $self->{targets} = @$configuration{'targets'};
+  $self->{exclude_patterns} = $configuration->{'exclude_patterns'};
+  $self->{files} = $configuration->{'files'};
+  $self->{hosts} = $configuration->{'hosts'};
+  $self->{local_directory} = '';
+  $self->{options} = $configuration->{'options'};
+  # Figure out which of the hosts is the source
+  my $current_host = shift || Sys::Hostname::hostname();
+  while ( my($key, $value) = each %{$self->{hosts}} ) {
+    # Match with current host if matches with nickname or
+    # full domain name
+    if ( $key eq $current_host || $value->{'domain'} eq $current_host ) {
+      $self->{local_directory} = $value->{'directory'};
+      delete $self->{hosts}->{$key};
+    }
+  }
   return $self;
 }
 
-sub filename {
-  my $self = shift @_;
-  return $self->{filename};
-}
-
 sub configuration {
-  my $self = shift @_;
+  my $self = shift;
   return $self->{configuration};
 }
 
+sub files {
+  my $self = shift;
+  return $self->{files};
+}
+
 sub options {
-  my $self = shift @_;
+  my $self = shift;
   my %options = %default_options;
   # Merge options from file with default options
   @options{ keys %{$self->{options}} } = values %{$self->{options}};
@@ -64,11 +75,12 @@ sub options {
 }
 
 sub options_list {
-  my $self = shift @_;
+  my $self = shift;
   my @list = ();
   my $options = $self->options;
   while ( my($key, $value) = each %$options ) {
     next unless $value;
+    $key = 'dry-run' if $key eq 'dry_run';
     if ( $value ne 0 ) {
       if ( $value ne 1 ) {
         push( @list, '--' . $key . '=' . $value );
@@ -80,40 +92,27 @@ sub options_list {
   return join( ' ', @list );
 }
 
-sub targets {
+sub hosts {
   my $self = shift @_;
-  return $self->{targets};
+  return $self->{hosts};
 }
 
 sub exclude_patterns {
-  my $self = shift @_;
+  my $self = shift;
   return $self->{exclude_patterns};
 }
 
-sub local_directory {
-  my $self = shift @_;
-  return $self->{local_directory};
-}
-
 sub sync {
-  my $self = shift @_;
-  #print $self->filename . "\n";
-  #print Dumper $self->configuration;
-  #print Dumper $self->options;
-  #print Dumper $self->targets;
-  #print Dumper $self->exclude_patterns;
-  #print Dumper $self->local_directory;
-  #print Dumper $self->options_list;
+  my $self = shift;
   print $self->sync_command . "\n";
 }
 
 sub sync_command {
-  my $self = shift @_;
+  my $self = shift;
   my @commands = ();
-  my $targets = $self->targets;
   foreach ( ('put', 'get', 'put') ) {
     my $method = $_ . '_command';
-    while ( my($key, $value) = each %$targets ) {
+    while ( my($key, $value) = each %{$self->hosts} ) {
       # Necessary?
       # no strict "refs";
       push( @commands, $self->$method( $value ) );
@@ -122,51 +121,47 @@ sub sync_command {
   return join( "; \\\n", @commands );
 }
 
-sub source_path {
-  my $self = shift @_;
-  my $configuration = $self->configuration;
-  return @$configuration{'source_path'};
+sub local_directory {
+  my $self = shift;
+  return $self->{local_directory};
 }
 
-sub base_name {
-  my $self = shift @_;
-  unless ( open SOURCE_FILE, '<', $self->source_path ) {
-    die "Cannot open source path: $!";
-  }
-  close SOURCE_FILE;
-  return File::Basename::basename( $self->source_path );
-}
-
-sub source_directory {
-  my $self = shift @_;
-  return File::Basename::dirname( $self->source_path );
-}
-
-sub target_path {
-  my $self = shift @_;
-  my $target = shift @_;
-  return $self->target_directory( $target ) . '/' . $self->base_name;
-}
-
-sub target_directory {
-  my $self = shift @_;
-  my $target = shift @_;
-  return @$target{'user'} . '@' . @$target{'host'} . ':' .
-    @$target{'directory'};
+sub remote_directory {
+  my $self = shift;
+  my $host = shift;
+  return $host->{'user'} . '@' . $host->{'domain'} . ':' . $host->{'directory'};
 }
 
 sub get_command {
-  my $self = shift @_;
-  my $target = shift @_;
-  return 'rsync ' . $self->options_list . ' ' . $self->source_path( $target ) .
-    ' ' . $self->target_directory( $target );
+  my $self = shift;
+  my $host = shift;
+  return &rsync_command(
+    $self->remote_directory($host),
+    $self->local_directory,
+    $self->files,
+    $self->options_list
+  ); 
 }
 
 sub put_command {
-  my $self = shift @_;
-  my $target = shift @_;
-  return 'rsync ' . $self->options_list . ' ' . $self->target_path( $target ) .
-    ' ' . $self->source_directory( $target );
+  my $self = shift;
+  my $host = shift;
+  return &rsync_command(
+    $self->local_directory,
+    $self->remote_directory($host),
+    $self->files,
+    $self->options_list
+  ); 
+}
+
+sub rsync_command {
+  my ( $source_directory, $target_directory, $files, $options_list ) = @_;
+  my @paths = ();
+  foreach ( @$files ) {
+    push( @paths, $source_directory . '/' . $_ );
+  }
+  my $path_list = join( ' ', @paths );
+  return 'rsync ' . $options_list . ' ' . $path_list . ' ' . $target_directory;
 }
 
 1

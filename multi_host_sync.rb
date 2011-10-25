13b9cdf23eb9900cfb027baa04195d51640ae5d0
Brian Warren <brian.scott.warren@gmail.com>
2011-10-25 15:32:11 -0500

    incorporating ruby optparse

--- multi_host_sync.rb
+++ multi_host_sync.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/ruby
 
+require 'optparse'
+require 'ostruct'
 require 'yaml'
 
 class MultiHostSync
@@ -8,7 +10,89 @@ class MultiHostSync
   attr_accessor :configuration
   attr_accessor :targets
 
+  def self.get_options(args)
+    options = OpenStruct.new
+    options.compress = true
+    options.dry_run = false
+    options.exclude_pattern = []
+    options.progress = true
+    options.recursive = true
+    options.rsh = '"ssh -p22"'
+    options.update_files = true
+    options.verbose = false
+
+    opts = OptionParser.new do |opts|
+      opts.banner = "Usage: multi_host_sync.rb [options]"
+      
+      opts.separator ''
+
+      opts.separator 'Specific options:'
+
+      opts.on( '-z', '--compress',
+               'compress file data during the transfer' ) do |comp|
+      opts.on( '-n', '--dry-run',
+               'show what would have been transferred' ) do |dry|
+        options.dry_run = true
+      end
+
+      opts.on( '--exclude=[PATTERN]',
+               'exclude files matching PATTERN' ) do |excl|
+        options.exclude_pattern << excl
+      end
+
+      opts.on( '--progress', 'show progress during transfer' ) do |prg|
+        options.prg = prg
+      end
+
+      opts.on( '-r', '--recursive', 'recurse into directories' ) do |rec|
+        options.rec = rec
+      end
+
+      opts.on( '-e', '--rsh=COMMAND',
+               'specify the remote shell to use' ) do |rsh|
+        options.rsh = rsh
+      end
+
+      opts.on( '-u', '--update',
+               'skip files that are newer on the receiver' ) do |updt|
+        options.update_files = updt
+      end
+
+      opts.on( '-v', '--verbose', 'increase verbosity' ) do |vrb|
+        options.verbose = vrb
+      end
+
+      opts.separator ''
+      opts.separator 'Common options:'
+
+      # No argument, shows at tail.  This will print an options summary.
+      # Try it and see!
+      opts.on_tail("-h", "--help", "Show this message") do
+        puts opts
+        exit
+      end
+
+      # Another typical switch to print the version.
+      opts.on_tail("--version", "Show version") do
+        puts OptionParser::Version.join('.')
+        exit
+      end
+    end
+  end
+
   def initialize( configuration_file = nil )
+    options = {}
+    OptionParser.new do |opts|
+      opts.banner = "Usage: example.rb [options]"
+
+      opts.on("-v", "--[no-]verbose", "Run verbosely") do
+        options[:verbose] = v
+      end
+    end.parse!
+
+    p options
+    p ARGV
+
     @configuration = configuration_from_file( configuration_file )
     # TODO: allow overriding configuration via command line arguments
     options = {}
